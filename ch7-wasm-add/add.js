4a86eca09e342f6f679b2370a4896019a9dd6c85
ammo <ammo@noobuntu.lan>
2022-07-20 09:04:32 +0630

    wasm

--- ch7-wasm-add/add.js
+++ ch7-wasm-add/add.js
@@ -1,7 +1,7 @@
 const fs = require('fs/promises');
 
 (async () => {
-    const wasm = fs.readFile('./add.wasm');
+    const wasm = await fs.readFile('./add.wasm');
     const { instance: { exports: { add } } } = await WebAssembly.instantiate(wasm);
     console.log(add(2, 3));
 })();
